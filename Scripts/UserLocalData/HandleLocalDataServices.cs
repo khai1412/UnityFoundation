7f843a3a3b51a6a9784ebc30793b2e532662809e
khai1412 <khai14122002123@gmail.com>
2023-07-18 11:44:45 +0700

    update fire bullet and add more animation of enemy

--- Scripts/UserLocalData/HandleLocalDataServices.cs
+++ Scripts/UserLocalData/HandleLocalDataServices.cs
@@ -14,30 +14,30 @@ namespace UnityFoundation.Scripts.UserLocalData
     /// </summary>
     public class HandleLocalDataServices
     {
-        
-        private readonly Dictionary<string, ILocalData> localDataCaches = new();
-        public void AddOrUpdateLocalData(string key,ILocalData localData)
+        private Dictionary<string, ILocalData> localDataCaches = new();
+        public void AddOrUpdateLocalData(string key, ILocalData localData)
         {
             if (this.localDataCaches.TryGetValue(key, out var data))
             {
                 this.localDataCaches[key] = data;
                 return;
             }
-            this.localDataCaches.Add(key,localData);
+
+            this.localDataCaches.Add(key, localData);
         }
-        
+
 
         public void SaveLocalData<T>(T localData) where T : ILocalData
         {
             var key = typeof(T).Name;
-            this.AddOrUpdateLocalData(key,localData);
+            this.AddOrUpdateLocalData(key, localData);
             var json = JsonConvert.SerializeObject(localData);
             PlayerPrefs.SetString(key, json);
             Debug.Log("Save " + key + ": " + json);
             PlayerPrefs.Save();
         }
-        
-        public ILocalData Load(Type type) 
+
+        public ILocalData Load(Type type)
         {
             var key = type.Name;
 
@@ -45,24 +45,25 @@ namespace UnityFoundation.Scripts.UserLocalData
             {
                 return cache;
             }
+
             var json = PlayerPrefs.GetString(key);
 
-            var result = string.IsNullOrEmpty(json) ? this.GetCurrentContainer().Resolve(type) : JsonConvert.DeserializeObject(json,type);
-            
-            this.AddOrUpdateLocalData(key,(ILocalData)result);
+            var result = string.IsNullOrEmpty(json) ? this.GetCurrentContainer().Resolve(type) : JsonConvert.DeserializeObject(json, type);
+
+            this.AddOrUpdateLocalData(key, (ILocalData)result);
             return (ILocalData)result;
         }
 
-        
+
         public void StoreAllToLocal()
         {
             foreach (var localData in this.localDataCaches)
             {
                 PlayerPrefs.SetString(localData.Key, JsonConvert.SerializeObject(localData.Value));
             }
+
             PlayerPrefs.Save();
             Debug.Log("Save Data To File");
         }
-        
     }
 }
